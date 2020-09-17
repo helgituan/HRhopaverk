7aeb8b9e5b02fbed07756a0edc8a46280ab155ef
helgituan <helgihe268@nemi.tskoli.is>
2020-09-17 11:27:23 +0000

    smá komið

--- test.py
+++ test.py
@@ -1,21 +1,32 @@
-#Leikður er í maze með 9 reitum, það er takmarkað hvernig hann getur hreyft sig 
-#á hvernjum stað sem hann er á fær hann upp hvert hann getur hreyft sig "You can travel: "
-# þarf að velja einn möguleika (n,e,s,w), ef hann velur eitthvað sem er ekki í boði þá fær hann "Not a valid direction!"
-#   7   8   9
-#   4   5   6
-#   1   2   3
+row1 = "ES/EW/WS/"
+row2 = "NES/WS/NS/"
+row3 = "N/N/V/"
 
-def tile_travel(inpitið?)
-    tile_1 = "n" or "N"
-    tile_2 = "n" or "N"
-    tile_3 = "n" or "N" #Victory 
-    tile_4 = "n" or "N" or "s" or "S" or "e" or "E"
-    tile_5 = "s" or "S" or "w" or "W"
-    tile_6 = "n" or "N" or "s" or "S"
-    tile_7 = "s" or "S" or "e" or "E"
-    tile_8 = "e" or "E" or "w" or "W"
-    tile_9 = "s" or "S" or "e" or "E"
+indexY = 0
+indexX = 0
 
-val = 1 
-while val:
-    if 
+def position(indexX,row):
+    counter = -1
+    idx = 0
+    last = 0
+    for i in row:
+        if i == "/":
+            counter += 1
+            if counter == indexX:
+                return row[last:idx]
+            else:
+                last = idx
+    idx += 1
+
+
+
+
+def getRow(indexY,row1,row2,row3):
+    if indexY == 0:
+        return row3
+    elif indexY == 1:
+        return row2
+    elif indexY == 2:
+        return row1
+
+def printDirections()
